8fa9da7ae1e368a602a490f8a7f173ec9a146ae0
Todd Jordan <todd.jordan@inin.com>
2015-10-01 10:33:56 -0400

    Update location indicator test per new register api

--- tests/integration/components/location-indicator-test.js
+++ tests/integration/components/location-indicator-test.js
@@ -2,12 +2,17 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
+let locationService;
 const locationStub = Ember.Service.extend({
   city: 'New York',
   country: 'USA',
   currentLocation: {
     x: 1234,
     y: 5678
+  },
+  init() {
+    this._super(...arguments);
+    locationService = this;
   },
   getCurrentCity() {
     return this.get('city');
@@ -21,7 +26,7 @@ moduleForComponent('location-indicator', 'Integration | Component | location ind
   integration: true,
 
   beforeEach() {
-    this.registry.register('service:location-service', locationStub);
+    this.register('service:location-service', locationStub);
   }
 });
 
@@ -33,7 +38,6 @@ test('should reveal current location', function(assert) {
 test('should change displayed location when current location changes', function(assert) {
   this.render(hbs`{{location-indicator}}`);
   assert.equal(this.$().text().trim(), 'You currently are located in New York, USA', 'origin location should display');
-  let locationService = this.container.lookup('service:location-service');
   Ember.run(() => {
     locationService.set('city', 'Beijing');
     locationService.set('country', 'China');
